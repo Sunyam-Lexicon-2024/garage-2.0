8ef49a6e948d71e2a62a21c8165f151c7fe1b6c4
Ezeih Alrashdan <ezeih.alrashdan@gmail.com>
2024-04-18 13:55:53 +0200

    skapat en vehicle create action + view

--- garage-2.0/Controllers/VehicleController.cs
+++ garage-2.0/Controllers/VehicleController.cs
@@ -1,12 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using garage_2._0.Models;
+
+
 
 namespace garage_2._0.Controllers
 {
     public class VehicleController : Controller
     {
-        public IActionResult List()
+        private readonly GarageDbContext _context;
+
+        public VehicleController(GarageDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vehicle/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Vehicle/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RegistrationNumber, Type, Brand, Model, Wheels, RegisteredAt, Color")] ParkedVehicle viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var vehicle = new ParkedVehicle
+                {
+                    RegistrationNumber = viewModel.RegistrationNumber,
+                    Type = viewModel.Type,
+                    Brand = viewModel.Brand,
+                    Model = viewModel.Model,
+                    Wheels = viewModel.Wheels,
+                    RegisteredAt = viewModel.RegisteredAt,
+                    Color = viewModel.Color
+                };
+
+                _context.ParkedVehicles.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        // GET: Vehicle/Index
+        public IActionResult Index()
+        {
+            var vehicles = _context.ParkedVehicles.ToList();
+            return View(vehicles);
         }
     }
 }
