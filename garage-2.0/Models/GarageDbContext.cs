cea8788594183cfd2377737f2cbe3deaf9c2e1eb
John Källström <johnkaellstrom@gmail.com>
2024-04-18 18:01:43 +0200

    Seed vehicles data

--- garage-2.0/Models/GarageDbContext.cs
+++ garage-2.0/Models/GarageDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using garage_2._0.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace garage_2._0.Models
 {
@@ -10,5 +11,69 @@ namespace garage_2._0.Models
 
         public DbSet<ParkedVehicle> ParkedVehicles { get; set; }
         public DbSet<Garage> Garages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var vehicles = new List<ParkedVehicle>
+            {
+                new ParkedVehicle
+                {
+                    Id = 1,
+                    RegistrationNumber = "FPD941",
+                    Type = VehicleType.Car,
+                    Brand = "Volkswagen",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Purple
+                },
+                new ParkedVehicle
+                {
+                    Id = 2,
+                    RegistrationNumber = "CLQ415",
+                    Type = VehicleType.Car,
+                    Brand = "Saab",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Yellow
+                },
+                new ParkedVehicle
+                {
+                    Id = 3,
+                    RegistrationNumber = "YHV901",
+                    Type = VehicleType.Car,
+                    Brand = "Volvo",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Blue
+                },
+                new ParkedVehicle
+                {
+                    Id = 4,
+                    RegistrationNumber = "GBO781",
+                    Type = VehicleType.Car,
+                    Brand = "Audi",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Black
+                },
+                new ParkedVehicle
+                {
+                    Id = 5,
+                    RegistrationNumber = "JRC132",
+                    Type = VehicleType.Car,
+                    Brand = "Toyota",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Green
+                },
+            };
+
+            modelBuilder.Entity<ParkedVehicle>().HasData(vehicles);
+        }
     }
 }
