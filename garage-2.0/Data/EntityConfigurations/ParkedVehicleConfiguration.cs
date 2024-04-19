87d80be8f8c6474fdfe8c32413ab1b66208d8f37
John Källström <johnkaellstrom@gmail.com>
2024-04-19 11:09:03 +0200

    Moved seed data to separate classews

---
+++ garage-2.0/Data/EntityConfigurations/ParkedVehicleConfiguration.cs
@@ -0,0 +1,80 @@
+﻿using garage_2._0.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace garage_2._0.Data.EntityConfigurations
+{
+    public class ParkedVehicleConfiguration : IEntityTypeConfiguration<ParkedVehicle>
+    {
+        public void Configure(EntityTypeBuilder<ParkedVehicle> builder)
+        {
+            builder.ToTable("ParkedVehicle");
+
+            var vehicles = new List<ParkedVehicle>
+            {
+                new ParkedVehicle
+                {
+                    Id = 1,
+                    GarageId = 1,
+                    RegistrationNumber = "FPD941",
+                    Type = VehicleType.Car,
+                    Brand = "Volkswagen",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Purple
+                },
+                new ParkedVehicle
+                {
+                    Id = 2,
+                    GarageId = 1,
+                    RegistrationNumber = "CLQ415",
+                    Type = VehicleType.Car,
+                    Brand = "Saab",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Yellow
+                },
+                new ParkedVehicle
+                {
+                    Id = 3,
+                    GarageId = 1,
+                    RegistrationNumber = "YHV901",
+                    Type = VehicleType.Car,
+                    Brand = "Volvo",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Blue
+                },
+                new ParkedVehicle
+                {
+                    Id = 4,
+                    GarageId = 1,
+                    RegistrationNumber = "GBO781",
+                    Type = VehicleType.Car,
+                    Brand = "Audi",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Black
+                },
+                new ParkedVehicle
+                {
+                    Id = 5,
+                    GarageId = 1,
+                    RegistrationNumber = "JRC132",
+                    Type = VehicleType.Car,
+                    Brand = "Toyota",
+                    Model = "Unknown",
+                    Wheels = 4,
+                    RegisteredAt = DateTime.Now,
+                    Color = Color.Green
+                },
+            };
+
+            builder.HasData(vehicles);
+        }
+    }
+}
--- garage-2.0/Models/GarageDbContext.cs
+++ garage-2.0/Models/GarageDbContext.cs
@@ -1,4 +1,4 @@
-﻿using garage_2._0.Enums;
+﻿using garage_2._0.Data.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace garage_2._0.Models
@@ -14,86 +14,8 @@ namespace garage_2._0.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Garage>().ToTable("Garage");
-            modelBuilder.Entity<Garage>().HasData(new Garage
-            {
-                ID = 1,
-                Name = "Default Garage One",
-                MaxCapacity = 50,
-            });
-
-            modelBuilder.Entity<ParkedVehicle>().ToTable("ParkedVehicle");
-            var vehicles = GetParkedVehiclesData();
-            modelBuilder.Entity<ParkedVehicle>().HasData(vehicles);
-        }
-
-        private List<ParkedVehicle> GetParkedVehiclesData()
-        {
-            var vehicles = new List<ParkedVehicle>
-            {
-                new ParkedVehicle
-                {
-                    Id = 1,
-                    GarageId = 1,
-                    RegistrationNumber = "FPD941",
-                    Type = VehicleType.Car,
-                    Brand = "Volkswagen",
-                    Model = "Unknown",
-                    Wheels = 4,
-                    RegisteredAt = DateTime.Now,
-                    Color = Color.Purple
-                },
-                new ParkedVehicle
-                {
-                    Id = 2,
-                    GarageId = 1,
-                    RegistrationNumber = "CLQ415",
-                    Type = VehicleType.Car,
-                    Brand = "Saab",
-                    Model = "Unknown",
-                    Wheels = 4,
-                    RegisteredAt = DateTime.Now,
-                    Color = Color.Yellow
-                },
-                new ParkedVehicle
-                {
-                    Id = 3,
-                    GarageId = 1,
-                    RegistrationNumber = "YHV901",
-                    Type = VehicleType.Car,
-                    Brand = "Volvo",
-                    Model = "Unknown",
-                    Wheels = 4,
-                    RegisteredAt = DateTime.Now,
-                    Color = Color.Blue
-                },
-                new ParkedVehicle
-                {
-                    Id = 4,
-                    GarageId = 1,
-                    RegistrationNumber = "GBO781",
-                    Type = VehicleType.Car,
-                    Brand = "Audi",
-                    Model = "Unknown",
-                    Wheels = 4,
-                    RegisteredAt = DateTime.Now,
-                    Color = Color.Black
-                },
-                new ParkedVehicle
-                {
-                    Id = 5,
-                    GarageId = 1,
-                    RegistrationNumber = "JRC132",
-                    Type = VehicleType.Car,
-                    Brand = "Toyota",
-                    Model = "Unknown",
-                    Wheels = 4,
-                    RegisteredAt = DateTime.Now,
-                    Color = Color.Green
-                },
-            };
-
-            return vehicles;
+            modelBuilder.ApplyConfiguration(new GarageConfiguration());
+            modelBuilder.ApplyConfiguration(new ParkedVehicleConfiguration());
         }
     }
 }
